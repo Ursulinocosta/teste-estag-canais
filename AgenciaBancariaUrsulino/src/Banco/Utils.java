c83c1872dce209eaf0ffeba701cd0517b8d7b399
Ursulino Costa <71571163+Ursulinocosta@users.noreply.github.com>
2022-05-09 14:27:23 -0200

    First Commit
    Primeiro Commit do banco

---
+++ AgenciaBancariaUrsulino/src/Banco/Utils.java
@@ -0,0 +1,15 @@
+//ações de formatação de valores
+
+package Banco;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+public class Utils {
+
+    static NumberFormat formatandoNumeros = new DecimalFormat("R$ #,##0.00");
+    
+    public static String doubleToString(Double valor) {
+        return Utils.formatandoNumeros.format(valor);
+        }
+}
